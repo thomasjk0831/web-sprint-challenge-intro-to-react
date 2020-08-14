2d1013b840984330c95cd04695bfe2d3ff99f1fe
Thomas Kim <Thomasjk0831@gmail.com>
2020-08-14 12:05:26 -0400

    thomas-kim

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+// Write your Character component here
+import React, { useState, useEffect} from 'react'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+    border: 3px solid black;
+    border-radius : 20px;
+    margin: 5px 5px;
+`
+
+function Character(props){
+
+    const { name, gender, height } = props
+    return(
+       <StyledDiv>
+           <h3>{name}</h3>
+           <p>gender: {gender}</p>
+           <p>height: {height}</p>
+       </StyledDiv>
+    )
+}
+
+export default Character
